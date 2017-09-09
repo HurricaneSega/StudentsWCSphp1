2ed968177749167a867a45e9e94ff9594e6537ee
Hurricane8 <paradox-sales19@live.fr>
2017-09-09 12:14:21 +0200

    Update StudentsWCSages.php

--- StudentsWCSages.php
+++ StudentsWCSages.php
@@ -7,7 +7,7 @@
  * Time: 09:56
  */
 
-// troizieme essai avec un "foreach" etla fonction "array" afin d'ergonomiser mon code
+// troizieme essai avec une fonction  "foreach" et la fonction "array" afin de raccourcir mon code par rapport au precedent
 //
 $students = array("Emmanuel" => "42", "Thierry" => "51", "Pascal" => "45", "Eric" => "48", "Nicolas" => "19");
     foreach($students as $k=>$v){
@@ -15,7 +15,10 @@ $students = array("Emmanuel" => "42", "Thierry" => "51", "Pascal" => "45", "Eric
     }
 // ici dessus echo me mettra en vue "$k" qui sera ma valeur clé donc les étudiants
 //et "$v" donnera ma valeur qui sera l'age.
+
     $moyenne = array_sum($students)/count($students);
+
 // donc au resultats final la moyenne sera le total de mes valeur "$students" avec la fonction "array_sum"
 //diviser par le nombre d'étudiant avec "count".
+
 echo "La moyenne d'âge des étudiants est de <strong> $moyenne </strong> ans.";
